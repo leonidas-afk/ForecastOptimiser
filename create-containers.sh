995582ae2c4d001a6e54fd5f5d20c0737e392ebe
leonidas-afk <llymberopoulos@hotmail.com>
2024-10-25 23:24:09 +0300

    ingress frontend localapi

--- create-containers.sh
+++ create-containers.sh
@@ -2,21 +2,21 @@
 
 VERSION_STREAM=$(git rev-parse --short HEAD) #or add your version here
 
-REPOSITORY=docker.io/lamdaleon
+REPOSITORY=docker.io/lamdaleon #public repo
 
 
-docker build -t $REPOSITORY/nginx-ingress:$VERSION_STREAM -f ./Dockerfile_nginx ./src
-docker push $REPOSITORY/nginx-ingress:$VERSION_STREAM
+# docker build -t $REPOSITORY/nginx-ingress:$VERSION_STREAM -f ./Dockerfile_nginx ./src
+# docker push $REPOSITORY/nginx-ingress:$VERSION_STREAM
+#
+# docker build -t $REPOSITORY/frontend:$VERSION_STREAM -f ./src/frontend/Dockerfile ./src/frontend
+# docker push $REPOSITORY/frontend:$VERSION_STREAM
+#
+# docker build -t $REPOSITORY/localapi:$VERSION_STREAM -f ./src/localapi/Dockerfile ./src/localapi/
+# docker push $REPOSITORY/localapi:$VERSION_STREAM
 
-docker build -t $REPOSITORY/frontend:$VERSION_STREAM -f ./pvapp/frontend/Dockerfile ./pvapp/frontend
-docker push $REPOSITORY/frontend:$VERSION_STREAM
+docker build -t $REPOSITORY/targetx-api-server:$VERSION_STREAM -f ./Dockerfile_api_server ./
+docker push $REPOSITORY/targetx-api-server:$VERSION_STREAM
 
-docker build -t $REPOSITORY/localapi:$VERSION_STREAM -f ./pvapp/localapi/Dockerfile ./pvapp/localapi/
-docker push $REPOSITORY/localapi:$VERSION_STREAM
 
-# docker build -t $REPOSITORY/targetx-api-server:$VERSION_STREAM -f ./Dockerfile_api_server ./src
-# docker push $REPOSITORY/targetx-api-server:$VERSION_STREAM
-
-# To train your model, you need to add the raw data in folder powerData. You will need to modify the target-trainer.py according to your data format
 # docker build -t $REPOSITORY/trainer:$VERSION_STREAM -f ./Dockerfile_trainer ./src
 # docker push $REPOSITORY/trainer:$VERSION_STREAM
