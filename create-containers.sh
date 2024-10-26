ab774356f4ff356689be27de04e54c28bdf7e6c9
leonidas-afk <llymberopoulos@hotmail.com>
2024-10-26 09:58:52 +0300

    minor update

--- create-containers.sh
+++ create-containers.sh
@@ -8,14 +8,14 @@ REPOSITORY=docker.io/lamdaleon #public repo
 # docker build -t $REPOSITORY/nginx-ingress:$VERSION_STREAM -f ./Dockerfile_nginx ./src
 # docker push $REPOSITORY/nginx-ingress:$VERSION_STREAM
 #
-# docker build -t $REPOSITORY/frontend:$VERSION_STREAM -f ./src/frontend/Dockerfile ./src/frontend
-# docker push $REPOSITORY/frontend:$VERSION_STREAM
+docker build -t $REPOSITORY/frontend:$VERSION_STREAM -f ./src/frontend/Dockerfile ./src/frontend
+docker push $REPOSITORY/frontend:$VERSION_STREAM
 #
 # docker build -t $REPOSITORY/localapi:$VERSION_STREAM -f ./src/localapi/Dockerfile ./src/localapi/
 # docker push $REPOSITORY/localapi:$VERSION_STREAM
 
-docker build -t $REPOSITORY/targetx-api-server:$VERSION_STREAM -f ./Dockerfile_api_server ./
-docker push $REPOSITORY/targetx-api-server:$VERSION_STREAM
+# docker build -t $REPOSITORY/targetx-api-server:$VERSION_STREAM -f ./Dockerfile_api_server ./
+# docker push $REPOSITORY/targetx-api-server:$VERSION_STREAM
 
 
 # docker build -t $REPOSITORY/trainer:$VERSION_STREAM -f ./Dockerfile_trainer ./src
